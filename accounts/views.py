d8ccc73ad15f1c59b0802bf8e31673e08cd77cf8
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 11:45:39 +0200

    added login capability

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 def login_view(request):
+    if request.method == 'POST':
+        usr = request.POST.get('usr')
+        pwd = request.POST.get('pwd')
+
+        userr = authenticate(request, username=usr, password=pwd)
+
+        if userr:
+            login(request, userr)
+            return HttpResponse('Logged In')
+        else:
+            return HttpResponse('Incorrect username/password!')
+
     return render(request, 'login.html')
 
 def signup_view(request):
@@ -10,7 +24,7 @@ def signup_view(request):
         usr = request.POST.get('usr')
         pwd = request.POST.get('pwd')
 
-        obj = User.objects.create(email=email, username=usr, password=pwd)
+        obj = User.objects.create_user(email=email, username=usr, password=pwd)
         obj.save()
 
         return redirect('login')
