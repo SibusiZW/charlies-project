7d0dc2ee1008322310af4ce2425a8da6390a322b
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 13:13:07 +0200

    added list display for hacks

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from .models import Hack
 
 @login_required(login_url='/auth/login')
 def home_view(request):
-    return render(request, 'home.html', { 'user': request.user })
+    hacks = Hack.objects.all().order_by('-created_at')
+
+    return render(request, 'home.html', { 'user': request.user, 'hacks': hacks })
 
 @login_required(login_url='/auth/login')
 def signout(request):
