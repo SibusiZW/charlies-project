a88633bd182b14137c2f16972598e0b69fee59ac
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 12:17:36 +0200

    edited Hack model

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Hack(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
