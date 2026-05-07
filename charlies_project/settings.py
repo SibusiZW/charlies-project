48a82c6cb8fc885e935915daef7d0459227da55a
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 13:32:55 +0200

    added prod adjustments

--- charlies_project/settings.py
+++ charlies_project/settings.py
@@ -17,8 +17,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
+CSRF_COOKIE_SECURE=True
+SESSION_COOKIE_SECURE=True
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com/'
+]
 
 # Application definition
 
