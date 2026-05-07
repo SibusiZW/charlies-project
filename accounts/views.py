18f9cc37a45819366f78835cb76bd551716968da
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 11:24:35 +0200

    added signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def login_view(request):
     return render(request, 'login.html')
 
 def signup_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        usr = request.POST.get('usr')
+        pwd = request.POST.get('pwd')
+
+        obj = User.objects.create(email=email, username=usr, password=pwd)
+        obj.save()
+
+        return redirect('login')
+
     return render(request, 'signup.html')
