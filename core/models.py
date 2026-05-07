0d58017aa154d7ffb55dbec28dca7da89b2cee19
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-05-07 12:12:46 +0200

    added Hack model and migrated to DB

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hack(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('title', 'user')
